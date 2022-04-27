e282845b7739483bb843563d76242c53991ad3a5
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2022-04-27 09:34:35 -0400

    Clarify parts needed for ROracle and RODBC. Remove < and >

--- R_code/project_logistics/R_credentials_sample.R
+++ R_code/project_logistics/R_credentials_sample.R
@@ -1,34 +1,38 @@
+# SECTION 1: THIS IS NEEDED TO CONNECT WITH RODBC and ROracle
+id<-"yourid"
+solepw<-"yoursole_pwd"
+novapw<-"yournova_pwd"
+# END SECTION 1: THIS IS NEEDED TO CONNECT WITH RODBC and ROracle
 
-id<-"<yourid>"
-solepw<-"<yoursole_pwd>"
-novapw<-"<yournova_pwd"
 
 
+# SECTION 2: THIS IS NEEDED TO CONNECT TO SOLE WITH ROracle
 
 drv<-dbDriver("Oracle")
-shost <- "<sole.full.path.to.server.gov>"
+shost <- "sole.full.path.to.server.gov"
 port <- port_number_here
-ssid <- "<ssid_here>"
+ssid <- "ssid_here"
 
 sole.connect.string<-paste(
   "(DESCRIPTION=",
   "(ADDRESS=(PROTOCOL=tcp)(HOST=", shost, ")(PORT=", port, "))",
   "(CONNECT_DATA=(SID=", ssid, ")))", sep="")
 
+# END SECTION 2: THIS IS NEEDED TO CONNECT TO SOLE WITH ROracle
 
-# Don't run this here.  You should put it in where you make the query and then close it when you are done with dbDisconnect(sole_conn)
-# sole_conn<-dbConnect(drv, id, password=solepw, dbname=sole.connect.string)
 
+# SECTION 3: THIS IS NEEDED TO CONNECT TO NOVA WITH ROracle
 
 
 nhost <- "nova.full.path.to.server.gov"
 port <- port_number_here
-ssid2 <- "<nova"
+ssid2 <- "nova"
 
 nova.connect.string<-paste(
   "(DESCRIPTION=",
   "(ADDRESS=(PROTOCOL=tcp)(HOST=", nhost, ")(PORT=", port, "))",
   "(CONNECT_DATA=(SID=", ssid2, ")))", sep="")
 
-# Don't run this here.  You should put it in where you make the query and then close it when you are done with dbDisconnect(nova_conn)
-# nova_conn<-dbConnect(drv, id, password=novapw, dbname=nova.connect.string)
+
+
+# END SECTION 3: THIS IS NEEDED TO CONNECT TO NOVA WITH ROracle
