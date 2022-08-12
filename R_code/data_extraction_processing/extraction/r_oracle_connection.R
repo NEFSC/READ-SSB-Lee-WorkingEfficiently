043ffb2822fdb346f18cdf42bf31f95ee05151d4
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2022-08-12 10:46:09 -0400

    tidy up references to R_credentials.build connection strings.

--- R_code/data_extraction_processing/extraction/r_oracle_connection.R
+++ R_code/data_extraction_processing/extraction/r_oracle_connection.R
@@ -1,8 +1,11 @@
 # This is code that uses Roracle to connect to oracle databases. 
 # installing the ROracle package is a little bit of a pain. 
-# 1. You need to have an Instantclient installation.
-# 2. You need to set the OCI_LIB64 system variable. You can do this with Sys.setenv(OCI_LIB64= "C:/instantclient_12_2") in your .Rprofile
-# 3. You will need to compile the ROracle package from source. The easiest way to do this is installing Rtools. 
+# 1. You need to have an Instantclient installation and sdk.  
+# 2. You need to set the OCI_LIB64 system variable. 
+  # You can do this temporarily with Sys.setenv(OCI_LIB64= "C:/instantclient_21_3") in your .Rprofile.
+  # A better way is just to have the environment variable set by ITD. 
+# 3. You may need to compile the ROracle package from source. The easiest way to do this is installing Rtools. 
+# I don't have  the sdk and so I cannot test this fully.
 
 library(here)
 library(ROracle)
@@ -12,15 +15,27 @@ here::i_am("R_code/data_extraction_processing/extraction/r_oracle_connection.R")
 
 my_projdir<-here()
 
-#this reads in paths and libraries
-source(file.path(my_projdir,"R_code","project_logistics","R_paths_libraries.R"))
-
-# This reads in your R credentials from R_credentials.R, which you have constructed from R_credentials_sample.R, and also added to .gitignore so your passwords are on github. 
-source(file.path(my_projdir,"R_code","project_logistics","R_credentials.R"))
-
  ############################################################################################
  #First, set up Oracle Connection
  ############################################################################################
+
+# set these variables somewhere that is not stored on github. I suggest your .Rprofile or .Renviron file.
+id<- yourid
+solepw<- yoursolepassword
+drv<-dbDriver("Oracle")
+shost <- "sole.full.path.to.server.gov"
+port <- port_number_here
+ssid <- "ssid_here"
+# You may need to use SERVICE_NAME instead of ssid.
+
+sole.connect.string<-paste(
+  "(DESCRIPTION=",
+  "(ADDRESS=(PROTOCOL=tcp)(HOST=", shost, ")(PORT=", port, "))",
+  "(CONNECT_DATA=(SID=", ssid, ")))", sep="")
+
+
+
+
 
 START.YEAR= 2017
 END.YEAR=2018
@@ -28,7 +43,6 @@ END.YEAR=2018
 #First, pull in permits and tripids into a list.
 permit_tripids<-list()
 i<-1
-
 
 for (years in START.YEAR:END.YEAR){
   sole_conn<-dbConnect(drv, id, password=solepw, dbname=sole.connect.string)
@@ -38,9 +52,7 @@ for (years in START.YEAR:END.YEAR){
   i<-i+1
 }
 #flatten the list into a dataframe
-
 permit_tripids<-do.call(rbind.data.frame, permit_tripids)
-colnames(permit_tripids)[which(names(permit_tripids) == "PERMIT")] <- "permit"
 
 
 
@@ -53,20 +65,3 @@ VTRgear<-dbGetQuery(sole_conn, querystring2)
 dbDisconnect(sole_conn)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
